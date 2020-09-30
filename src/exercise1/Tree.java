241f30d685a8c57f8eea73eb1508fb32505c711a
alisadermen <alisadermen@gmail.com>
2020-09-30 14:37:23 +0000

    tree

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -1,7 +1,7 @@
 package exercise1;
 public class Tree {
   public static void main(String[] args) {
-    System.out.print("    * \n");
+    System.out.print("    *\n");
     System.out.print("   ***\n");
     System.out.print("  *****\n");
     System.out.print(" *******\n");
